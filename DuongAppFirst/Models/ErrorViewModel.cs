3ee9f46bd4f8a9b3b52a53a93b9b156c41242238
Vu Duong <Duongvv10@outlook.com>
2021-08-12 17:09:50 +0700

    lesson 7 : Create Solution

---
+++ DuongAppFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DuongAppFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
